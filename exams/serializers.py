52ec8c08fd9d5f65f7eaeff956784786fbb605e3
Cypha <omarcypha@gmail.com>
2026-01-10 18:02:09 +0000

    prepare for hosting

--- exams/serializers.py
+++ exams/serializers.py
@@ -7,32 +7,23 @@ User = get_user_model()
 # ================ ADMIN DASHBOARD SERIALIZERS ==============
 
 class UserSerializer(serializers.ModelSerializer):
-    full_name = serializers.SerializerMethodField()
     
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'full_name', 'is_active', 'date_joined']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role', 'is_active', 'date_joined']
         read_only_fields = ['date_joined']
     
-    def get_full_name(self, obj):
-        return obj.get_full_name() or obj.username
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    password_confirm = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
     
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password_confirm']
+        fields = ['username', 'email', 'first_name', 'last_name', 'role', 'password',] #'password_confirm'
     
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password_confirm']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
     
     def create(self, validated_data):
-        validated_data.pop('password_confirm')
         user = User.objects.create_user(**validated_data)
         return user
 
